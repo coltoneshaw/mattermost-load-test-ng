eaeef220dabc874e26e5588966417574ccab8560
coltoneshaw <colton@theshaw.group>
2023-05-18 21:09:13 -0400

    added custom changes

--- deployment/terraform/assets/cluster.tf
+++ deployment/terraform/assets/cluster.tf
@@ -38,6 +38,7 @@ resource "aws_instance" "app_server" {
     host = self.public_ip
   }
 
+  iam_instance_profile = "ep-loadtest-s3-role"
   ami           = var.aws_ami
   instance_type = var.app_instance_type
   key_name      = aws_key_pair.key.id
@@ -230,17 +231,17 @@ resource "aws_elasticsearch_domain" "es_server" {
 
 resource "aws_iam_user" "s3user" {
   name  = "${var.cluster_name}-s3user"
-  count = var.app_instance_count > 1 ? 1 : 0
+  count = 0
 }
 
 resource "aws_iam_access_key" "s3key" {
   user  = aws_iam_user.s3user[0].name
-  count = var.app_instance_count > 1 ? 1 : 0
+  count = 0
 }
 
 resource "aws_s3_bucket" "s3bucket" {
   bucket = "${var.cluster_name}.s3bucket"
-  count  = var.app_instance_count > 1 ? 1 : 0
+  count  = 0
   tags = {
     Name = "${var.cluster_name}-s3bucket"
   }
@@ -251,7 +252,7 @@ resource "aws_s3_bucket" "s3bucket" {
 resource "aws_iam_user_policy" "s3userpolicy" {
   name  = "${var.cluster_name}-s3userpolicy"
   user  = aws_iam_user.s3user[0].name
-  count = var.app_instance_count > 1 ? 1 : 0
+  count = 0
 
   policy = <<EOF
 {
